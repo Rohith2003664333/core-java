c4a4c902361bbf7294b40ca47be5a2bb0bea47ab
Rohith <Rohith@Ram>
2024-06-25 19:01:38 +0530

    first commit

---
+++ corejava/src/com/tsnf/javaprogram/Encapsulation.java
@@ -0,0 +1,13 @@
+package com.tsnf.javaprogram;
+
+public class Encapsulation {
+	int serialNum;//Data members
+	String name;
+	int age;
+	
+	void show()//member function
+	{		System.out.println("serinum is "+serialNum+"\t"+"name"+"\t"+name+"\t"+"age "+age);
+	}
+	
+
+}
---
+++ corejava/src/com/tsnf/javaprogram/EncapsulationDemo.java
@@ -0,0 +1,12 @@
+package com.tsnf.javaprogram;
+
+public class EncapsulationDemo {
+	public static void main(String args[]) {
+		Encapsulation ob=new Encapsulation();
+		ob.name="Rohith";
+		ob.age=18;
+		ob.serialNum=72;
+		ob.show();
+	}
+
+}
